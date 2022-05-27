6a1879f90bccc8a95a48e9de980d9465708a2a77
zhenkai <zhenkai@staff.weibo.com>
2022-05-28 07:24:59 +0800

    add another solved method

--- rotate_array/rotate_array.cpp
+++ rotate_array/rotate_array.cpp
@@ -15,12 +15,47 @@ int sort(int *arr, int start, int end)
     }
 }
 
+int sort2(int *arr, int length)
+{
+    if (arr == NULL) {
+        throw;
+    }
+    int start = 0;
+    int end = length - 1;
+    int mid = 0;
+    while (arr[end] <= arr[start]) {
+        if (end == start + 1) {
+            return arr[end];
+        }
+
+        mid = (start + end) / 2;
+        if (arr[start] == arr[mid] && arr[mid] == arr[end]) {
+            int small = arr[start]; 
+            for (int i = start + 1; i < length; i++) {
+                if (arr[i] < small) {
+                    small = arr[i];
+                }
+            }
+            return small;
+        }
+
+        if (arr[start] >= arr[mid]) {
+            end = mid;
+        } else {
+            start = mid;
+        }
+    }
+
+    return arr[mid];
+}
 
 
 int main(void) {
 
-        int arr[] = {3,4,5,6,-1,1,2};
-        int a = sort(arr, 0, sizeof(arr) / sizeof(int) - 1);
+        //int arr[] = {3,4,5,6,-1,1,2};
+        int arr[] = {1,1,1,0,1,1};
+        //int a = sort(arr, 0, sizeof(arr) / sizeof(int) - 1);
+        int a = sort2(arr, sizeof(arr) / sizeof(int));
 
         std::cout << a << std::endl; 
 
